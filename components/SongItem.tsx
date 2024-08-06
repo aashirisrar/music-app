873b9f0360027bc9d537623c63ac099e72fa153a
John Doe <aashirisrar968@gmail.com>
2024-08-06 12:52:59 +0500

    added play button

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -6,6 +6,8 @@ import useLoadImage from "@/hooks/useLoadImage";
 
 import { Song } from "@/types/route";
 
+import PlayButton from "@/components/PlayButton";
+
 interface SongsItemProps {
   song: Song;
   onClick: (id: string) => void;
@@ -16,7 +18,7 @@ const SongItem: React.FC<SongsItemProps> = ({ song, onClick }) => {
 
   return (
     <div
-      onClick={() => onClick(song.id)}
+      onClick={() => onClick(song?.id)}
       className="relative group flex flex-col items-center justify-center rounded-md overflow-hidden gap-x-4 bg-neutral-400/5 cursor-pointer hover:bg-neutral-400/10 transition p-3"
     >
       <div className="relative aspect-square w-full h-full rounded-md overflow-hidden">
@@ -26,6 +28,15 @@ const SongItem: React.FC<SongsItemProps> = ({ song, onClick }) => {
           fill
           alt="Image"
         />
+      </div>
+      <div className="flex flex-col items-start w-full pt-4 gap-y-1">
+        <p className="font-semibold truncate w-full">{song?.title}</p>
+        <p className="text-neutral-400 text-sm pb-4 w-full truncate">
+          By {song?.author}
+        </p>
+      </div>
+      <div className="absolute bottom-24 right-5">
+        <PlayButton />
       </div>
     </div>
   );
